bcc9fb7cac593a191102d83201bbb8c2681e6ac7
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-03-11 10:18:42 +0100

    added ema fun

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import pandas
+import pandas as pd
 
-# reading the CSV file
-csvFile = pandas.read_csv('dane.csv')
+import csv
 
-# displaying the contents of the CSV file
-print(csvFile)
+def ema(N, p):
+    alfa = 1- 2/(N+1)
+
+    for i in range(p):
+        print(p)
+
+
+data = pd.read_csv('dane2.csv') #import data
+
+#print (data.loc[2:6,['open']])
+#print(csvFile) #print data
+
+
+
+
+
